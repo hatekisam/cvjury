0ae7b271dd367c1f70704b6e721265bf92fee38e
Mugisha Yves <mugishayves189000@gmail.com>
2023-10-24 07:44:37 +0200

    created the drag and drop input for resume

--- src/components/FileDrop.tsx
+++ src/components/FileDrop.tsx
@@ -2,13 +2,11 @@ import { FC, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 
 const FileDropZone: FC<{
-    fileType: string;
-    onFilesSelected: (filetype: string, files: File[]) => void;
-    title: string;
-}> = ({ fileType, onFilesSelected, title }) => {
+    onFilesSelected: (files: File[]) => void;
+}> = ({ onFilesSelected }) => {
     const [selectedImage, setSelectedImage] = useState<string | null>(null);
     const onDrop = (acceptedFiles: File[]) => {
-        onFilesSelected(fileType, acceptedFiles);
+        onFilesSelected(acceptedFiles);
         const file = acceptedFiles[0];
         if (file) {
             const reader = new FileReader();
@@ -47,7 +45,7 @@ const FileDropZone: FC<{
                     ) : (
                         <div className="bg-[rgba(67,67,67,0.03)] h-[300px] rounded-md border-[2px] border-[rgba(67,67,67,0.09)] flex flex-col gap-5 justify-center  items-center p-2">
                             <p className="text-[rgba(0,0,0,0.1)]  text-[28px] font-semibold">Drag & Drop</p>
-                            <p className="text-[rgba(73,73,74,0.78)] text-sm font-medium">{title}</p>
+                            <p className="text-[rgba(73,73,74,0.78)] text-sm font-medium"></p>
                         </div>
                     )}
                 </div>
--- src/components/ResumeUpload.tsx
+++ src/components/ResumeUpload.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import FileDropZone from './FileDrop'
 
 const ResumeUpload = () => {
+    const handleFiles = (files: File[]) => {
+        console.log(files);
+      };
     return (
         <div className='w-[50%] flex justify-between'>
             <div className='w-[50%]'></div>
             <div className='w-[50%] text-center' >
                 <p className='text-[#6A6A6A] text-3xl'>Resume</p>
-                <div className="p-5 border-dashed border-2 border-black rounded-3xl "></div>
+                <div className="p-5 border-dashed border-2 border-black rounded-3xl ">
+                    <FileDropZone onFilesSelected={handleFiles} />
+                </div>
             </div>
         </div>
     )
