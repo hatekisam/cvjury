c3f75338d29c303b338da60a406638ec3ec066fc
Mugisha Yves <mugishayves189000@gmail.com>
2023-10-24 07:59:06 +0200

    finished the resume component

--- src/components/ResumeUpload.tsx
+++ src/components/ResumeUpload.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import FileDropZone from './FileDrop'
 
-const ResumeUpload = () => {
+const ResumeUpload:React.FC = () => {
     const handleFiles = (files: File[]) => {
         console.log(files);
       };
@@ -12,6 +12,9 @@ const ResumeUpload = () => {
                 <p className='text-[#6A6A6A] text-3xl'>Resume</p>
                 <div className="p-5 border-dashed border-2 border-black rounded-3xl ">
                     <FileDropZone onFilesSelected={handleFiles} />
+                    <div className="mt-5">
+                        <textarea  className='border-[#BCBCBC] border rounded-lg p-3 text-[13px] w-full resize-none outline-none h-[200px]' placeholder='Or paste your resume here'></textarea>
+                    </div>
                 </div>
             </div>
         </div>
