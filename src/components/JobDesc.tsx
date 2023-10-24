065b62539e9b7ce7df1a64271dda746fdf7fcc77
Mugisha Yves <mugishayves189000@gmail.com>
2023-10-24 07:36:20 +0200

    created the job description component

--- src/components/JobDesc.tsx
+++ src/components/JobDesc.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const JobDesc = () => {
     return (
-        <div>JobDesc</div>
+        <div className='w-[50%] flex justify-between'>
+            <div className='w-[50%] text-center' >
+                <p className='text-[#6A6A6A] text-3xl'>Job Description</p>
+                <div className="p-5 border-dashed border-2 border-black rounded-3xl ">Resume</div>
+            </div>
+            <div className='w-[50%]'></div>
+        </div>
     )
 }
 
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Helmet from "react-helmet"
+import ResumeUpload from '../components/ResumeUpload'
+import JobDesc from '../components/JobDesc'
 const Main: React.FC = () => {
 	return (
 		<div className='relative flex flex-col items-center py-16 w-screen h-screen overflow-hidden'>
@@ -10,20 +12,8 @@ const Main: React.FC = () => {
 			<div className="absolute bg-[rgba(224,107,66,0.2)]  w-[50%] h-full right-0 top-0"></div>
 			<p className="font-medium text-4xl z-20 ">CVJury ATS Resume Checker at Work</p>
 			<div className="flex gap-5 items-center z-20  w-full justify-center mt-20">
-				<div className='w-[50%] flex justify-between'>
-					<div className='w-[50%]'></div>
-					<div className='w-[50%] text-center' >
-						<p className='text-[#6A6A6A] text-3xl'>Resume</p>
-						<div className="p-5 border-dashed border-2 border-black rounded-3xl ">Resume</div>
-					</div>
-				</div>
-				<div className='w-[50%] flex justify-between'>
-					<div className='w-[50%] text-center' >
-						<p className='text-[#6A6A6A] text-3xl'>Job Description</p>
-						<div className="p-5 border-dashed border-2 border-black rounded-3xl ">Resume</div>
-					</div>
-					<div className='w-[50%]'></div>
-				</div>
+				<ResumeUpload />
+				<JobDesc />
 			</div>
 		</div>
 	)
