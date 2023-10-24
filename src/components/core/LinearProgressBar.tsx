13cd7a950d8f80cc08e327a69aed15cb80aa75f5
Mugisha Yves <mugishayves189000@gmail.com>
2023-10-24 14:49:00 +0200

    Added the linear progress component

---
+++ src/components/core/LinearProgressBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+interface Props {
+    score: number
+}
+const LinearProgressBar: React.FC<Props> = ({ score }) => {
+    return (
+        <div className="w-full bg-[#D6D6D6] rounded-full my-3">
+            <div className={`w-full py-2  ${score < 20 ? "bg-red-500" : score < 30 ? "bg-[#a86750]" : "bg-[#E06B42]"} rounded-full`} style={{ width: `${score}%` }}></div>
+        </div>
+    )
+}
+
+export default LinearProgressBar
