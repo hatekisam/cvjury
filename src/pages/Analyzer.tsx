c96a80ae27e4113bdaff693b2b60a7677a248e29
Mugisha Yves <mugishayves189000@gmail.com>
2023-10-24 11:55:45 +0200

    finaly finished the tables on analyzer pages

--- src/pages/Analyzer.tsx
+++ src/pages/Analyzer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Table from '../components/core/Table';
-import { JobIcon, ResumeIcon } from '../components/core/icons';
+import { DesirableIcon, JobIcon, ResumeIcon } from '../components/core/icons';
 
 const Analyzer: React.FC = () => {
     const dummyTabs = [
@@ -47,6 +47,22 @@ const Analyzer: React.FC = () => {
                 <Table tabs={dummyTabs} data={dummyData} />
                 <div className="h-[40px]"></div>
                 <Table tabs={dummyTabs2} data={dummyData} />
+            </div>
+            <div className="my-10">
+                <div style={{ boxShadow: '0px 13px 50px 0px rgba(0, 0, 0, 0.15)' }} className=" bg-white border border-[#0078D4] text-[#0078D4] w-fit flex items-center gap-3 p-2 rounded-full">
+                    <DesirableIcon />
+                    <p className='text-[30px] pr-5'>SEE DESIRABLE SKILLS</p>
+                </div>
+                <p className='text-[24px] my-5'>P.S.− While explicit mentions are a straightforward way to identify skills, there are often implied or contextual skills that professionals might possess based on the nature of their work, even if these skills aren't directly mentioned. Using the provided resume and job description, some potential desirable skills and attributes can be inferred.When presenting these skills, especially in an interview or in a more detailed resume, it's essential to provide specific examples or experiences that demonstrate these attributes:</p>
+                <div className='my-10'>
+                    <div className="flex justify-between">
+                        <div></div>
+                        <p className='uppercase text-3xl'>Table 2 </p>
+                    </div>
+                    <Table tabs={dummyTabs} data={dummyData} />
+                    <div className="h-[40px]"></div>
+                    <Table tabs={dummyTabs2} data={dummyData} />
+                </div>
             </div>
         </div>
     )
