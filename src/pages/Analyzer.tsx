e5929b2de7ea87f5ef51999ca4188f71df78c1ea
Mugisha Yves <mugishayves189000@gmail.com>
2023-10-24 08:31:20 +0200

    started creating the analyzer page

--- src/pages/Analyzer.tsx
+++ src/pages/Analyzer.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Analyzer: React.FC = () => {
     return (
-        <div className='relative flex items-center justify-center w-'></div>
+        <div className='w-screen bg-[#F1F1F1] px-5 md:px-8 lg:px-12 py-10'>
+            <p>Analyzer</p>
+        </div>
     )
 }
 
