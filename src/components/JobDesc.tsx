c5f2bcebc665fadd99be30b4a81479eae8cd332f
Mugisha Yves <mugishayves189000@gmail.com>
2023-10-27 11:00:17 +0200

    changed the scan button as per design

--- src/components/JobDesc.tsx
+++ src/components/JobDesc.tsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import { DeleteIcon } from './core/icons'
+import React from "react";
+import { DeleteIcon } from "./core/icons";
 
 const JobDesc: React.FC = () => {
-    return (
-        <div className='w-full  sm:w-[50%] flex flex-col md:flex-row justify-between'>
-            <div className='w-full md:w-[80%] lg:w-[65%] text-center' >
-                <p className='text-[#6A6A6A] text-3xl'>Job Description</p>
-                <div className="p-5 border-dashed border-2 border-black rounded-3xl ">
-                    <input type="text" placeholder='Enter Job Title' className='border-[#BCBCBC] border rounded-lg p-3 text-[13px] w-full  outline-none ' />
-                    <div className="mt-5">
-                        <textarea className='border-[#BCBCBC] border rounded-lg p-3 text-[13px] w-full resize-none outline-none h-[200px]' placeholder='Or paste your resume here'></textarea>
-                    </div>
-                    <div className="flex mt-1 justify-between">
-                        <div></div>
-                        <button><DeleteIcon /></button>
-                    </div>
-                </div>
-            </div>
-            <div className='my-5 md:my-0 w-full md:w-[20%] lg:w-[35%] flex items-center justify-center text-xs'>
-                <button className='text-white bg-[#E06B42] px-4 py-2 rounded-full  border-[#C7CCD2] border'>Scan for free</button>
-            </div>
+  return (
+    <div className="w-full  sm:w-[50%] flex flex-col md:flex-row justify-between">
+      <div className="w-full md:w-[80%] lg:w-[65%] text-center">
+        <p className="text-[#6A6A6A] text-3xl">Job Description</p>
+        <div className="p-5 border-dashed border-2 border-black rounded-3xl ">
+          <input
+            type="text"
+            placeholder="Enter Job Title"
+            className="border-[#BCBCBC] border rounded-lg p-3 text-[13px] w-full  outline-none "
+          />
+          <div className="mt-5">
+            <textarea
+              className="border-[#BCBCBC] border rounded-lg p-3 text-[13px] w-full resize-none outline-none h-[200px]"
+              placeholder="Or paste your resume here"
+            ></textarea>
+          </div>
+          <div className="flex mt-1 justify-between">
+            <div></div>
+            <button>
+              <DeleteIcon />
+            </button>
+          </div>
         </div>
-    )
-}
+      </div>
+      <div className="my-5 md:my-0 w-full md:w-[20%] lg:w-[35%] flex items-center justify-center text-xs"></div>
+    </div>
+  );
+};
 
-export default JobDesc
+export default JobDesc;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import Helmet from "react-helmet"
-import ResumeUpload from '../components/ResumeUpload'
-import JobDesc from '../components/JobDesc'
-import { Logo } from '../components/core/icons'
+import React from "react";
+import Helmet from "react-helmet";
+import ResumeUpload from "../components/ResumeUpload";
+import JobDesc from "../components/JobDesc";
+import { Logo } from "../components/core/icons";
 const Main: React.FC = () => {
-	return (
-		<div className='relative flex flex-col items-center py-16 w-screen h-screen overflow-y-auto sm:overflow-hidden px-5 '>
-			<Helmet>
-				<title>CV Jury</title>
-			</Helmet>
-			<div className="absolute left-5 top-5 z-20">
-				<Logo />
-			</div>
-			<div className="hidden sm:block absolute bg-white w-[50%] h-full left-0 top-0"></div>
-			<div className="hidden sm:block absolute bg-[rgba(224,107,66,0.2)]  w-[50%] h-full right-0 top-0"></div>
-			<p className="font-medium text-3xl sm:text-4xl z-20 ">CVJury ATS Resume Checker at Work</p>
-			<div className="flex flex-col sm:flex-row gap-5 items-center z-20  w-full justify-center mt-20">
-				<ResumeUpload />
-				<JobDesc />
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="relative flex flex-col items-center py-16 w-screen h-screen overflow-y-auto sm:overflow-hidden px-5 ">
+      <Helmet>
+        <title>CV Jury</title>
+      </Helmet>
+      <div className="absolute left-5 top-5 z-20">
+        <Logo />
+      </div>
+      <div className="hidden sm:block absolute bg-white w-[50%] h-full left-0 top-0"></div>
+      <div className="hidden sm:block absolute bg-[rgba(224,107,66,0.2)]  w-[50%] h-full right-0 top-0"></div>
+      <p className="font-medium text-3xl sm:text-4xl z-20 ">
+        CVJury ATS Resume Checker at Work
+      </p>
+      <div className="flex flex-col sm:flex-row gap-5 items-center z-20  w-full justify-center mt-20">
+        <ResumeUpload />
+        <JobDesc />
+      </div>
+      <button className="my-3 z-20 text-white bg-[#E06B42] px-4 py-2 rounded-full  border-[#C7CCD2] border">
+        Scan for free
+      </button>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
