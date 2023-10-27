0963978cda909a2316aa0d7c62dbaaa8c58cb8bb
Mugisha Yves <mugishayves189000@gmail.com>
2023-10-27 14:57:43 +0200

    started making the second page responsive

--- src/components/core/icons.tsx
+++ src/components/core/icons.tsx
@@ -78,6 +78,7 @@ export const ResumeIcon = () => (
     height="50"
     viewBox="0 0 64 64"
     fill="none"
+    className="w-[30px] md:w-[45px] transition-all duration-200 "
   >
     <circle cx="32" cy="32" r="32" fill="#CBEEE3" />
     <path d="M26.7463 38.209H36.2985V36.2985H26.7463V38.209Z" fill="#29AB82" />
--- src/pages/Analyzer.tsx
+++ src/pages/Analyzer.tsx
@@ -137,7 +137,9 @@ const Analyzer: React.FC = () => {
         if (showingShare) setShowingShare(false);
       }}
     >
-      <p className="transition-all duration-150  text-[22px]  md:text-[32px] lg:text-[40px] my-2">Hard and Soft Skills (Frequencies)</p>
+      <p className="transition-all duration-150  text-[22px]  md:text-[32px] lg:text-[40px] my-2">
+        Hard and Soft Skills (Frequencies)
+      </p>
       <p className="transition-all duration-150  text-[16px]  md:text-[20px] lg:text-2xl my-2">
         Based on the resume and job description you uploaded, here are the hard
         and soft skills we extracted from them:
@@ -145,14 +147,18 @@ const Analyzer: React.FC = () => {
       <div className="my-10 relative">
         <div className="flex justify-between">
           <div></div>
-          <p className="uppercase text-3xl">Table 1 </p>
+          <p className="uppercase transition-all duration-150  text-[18px]  md:text-[24px] lg:text-3xl">
+            Table 1{" "}
+          </p>
         </div>
         <div
           style={{ boxShadow: "0px 13px 50px 0px rgba(0, 0, 0, 0.15)" }}
-          className="absolute left-3 top-2 bg-white border border-[#29AB82] text-[#29AB82] flex items-center gap-3 p-2 rounded-full"
+          className="absolute left-3 top-2 bg-white border border-[#29AB82] text-[#29AB82] flex items-center gap-3 py-0.5 md:py-2 p-2 rounded-full"
         >
           <ResumeIcon />
-          <p className="text-[30px] pr-5">RESUME</p>
+          <p className="transition-all duration-150  text-[18px]  md:text-[24px] lg:text-3xl pr-5">
+            RESUME
+          </p>
         </div>
         <Table tabs={dummyTabs} data={dummyData} />
         <div className="h-[40px]"></div>
