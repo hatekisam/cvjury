0459bb13b64e059295debdb01a69cff4835e2540
Mugisha Yves <mugishayves189000@gmail.com>
2023-10-27 11:10:46 +0200

    changed the scan button as per design

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,15 +5,15 @@ import JobDesc from "../components/JobDesc";
 import { Logo } from "../components/core/icons";
 const Main: React.FC = () => {
   return (
-    <div className="relative flex flex-col items-center py-16 w-screen h-screen overflow-y-auto sm:overflow-hidden px-5 ">
+    <div className="relative flex flex-col items-center py-16 w-screen h-screen overflow-y-auto  px-5 ">
       <Helmet>
         <title>CV Jury</title>
       </Helmet>
       <div className="absolute left-5 top-5 z-20">
         <Logo />
       </div>
-      <div className="hidden sm:block absolute bg-white w-[50%] h-full left-0 top-0"></div>
-      <div className="hidden sm:block absolute bg-[rgba(224,107,66,0.2)]  w-[50%] h-full right-0 top-0"></div>
+      <div className="hidden sm:block fixed bg-white w-[50%] h-full left-0 top-0"></div>
+      <div className="hidden sm:block fixed bg-[rgba(224,107,66,0.2)]  w-[50%] h-screen right-0 top-0"></div>
       <p className="font-medium text-3xl sm:text-4xl z-20 ">
         CVJury ATS Resume Checker at Work
       </p>
@@ -21,9 +21,19 @@ const Main: React.FC = () => {
         <ResumeUpload />
         <JobDesc />
       </div>
-      <button className="my-3 z-20 text-white bg-[#E06B42] px-4 py-2 rounded-full  border-[#C7CCD2] border">
+      <button className="my-3  z-20 text-white bg-[#E06B42] px-4 py-2 rounded-full  border-[#C7CCD2] border">
         Scan for free
       </button>
+	  <div className="bg-white opacity-20 w-[60vw] p-5 rounded-full">
+		<p className="text-black text-3xl">The 5 sets of results (from your resume and job description scan):</p>
+		<ul className="list-decimal">
+			<li>Table 1: Resume hard and soft skills (Frequencies)</li>
+			<li>Table 2: Job description hard and soft skills (Frequencies)</li>
+			<li>Table 3: Suggested desirable skills</li>
+			<li>Table 4: Job description and resume compared — Skills Gap Identified</li>
+			<li>Similarity Score</li>
+		</ul>
+	  </div>
     </div>
   );
 };
