2e69b45bcdda46b96e82dc0798574da6f3b32560
henritresor <shimwamanahenritresor@gmail.com>
2023-10-29 16:30:42 +0200

    added icons and fixed build

--- src/components/core/ColouredTable.tsx
+++ src/components/core/ColouredTable.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { FaRegCheckCircle, FaRegTimesCircle, FaBell } from 'react-icons/fa'
 
 interface Props {
   tabs: Tab[];
@@ -16,27 +16,29 @@ const ColouredTable: React.FC<Props> = ({ tabs, data }) => {
         {data.map((rowData, i) => (
           <tr
             key={i}
-            className={`text-right   ${
-              i == 0
-                ? "border-top-0"
-                : i == tabs.length - 1
+            className={`text-right   ${i == 0
+              ? "border-top-0"
+              : i == tabs.length - 1
                 ? "border-b-0"
                 : "border"
-            }`}
+              }`}
           >
             {tabs.map((tab, colIndex) => (
               <td
                 style={{ background: tab.background }}
                 key={colIndex}
-                className={`${
-                  !tab.background
-                    ? "border  border-[#E2DFDF]"
-                    : i == 0
+                className={`${!tab.background
+                  ? "border  border-[#E2DFDF]"
+                  : i == 0
                     ? "rounded-tl-[20px]"
                     : i === tabs.length - 1 && "rounded-bl-[20px]"
-                }  px-4 py-2`}
+                  }  px-4 py-2`}
               >
-                {rowData[tab.title] == true && tab.title == "Confirmed" ? (<div></div>):rowData[tab.title] ? rowData[tab.title] : "10"}
+                {rowData[tab.title] == true && tab.title == "Confirmed" ?
+                  (<div className="text-green-500">{<FaRegCheckCircle />}</div>) :
+                  tab.title == "Notification" ? (<div className="text-red-500">{<FaBell />}</div>) :
+                    tab.title == "Title" ? (<div className="text-red-500">{ tab.title}</div>) :
+                      rowData[tab.title] ? rowData[tab.title] : <div className="text-red-500"><FaRegTimesCircle /></div>}
               </td>
             ))}
           </tr>
