e1e5dcd839506ff947b68c5ecbda3da215a69ef4
Mugisha Yves <mugishayves189000@gmail.com>
2023-10-31 11:20:19 +0200

    made the coloured tables as design

--- src/components/core/ColouredTable.tsx
+++ src/components/core/ColouredTable.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React from "react";
 import { FaRegCheckCircle, FaRegTimesCircle, FaBell } from "react-icons/fa";
 
@@ -24,15 +25,16 @@ const ColouredTable: React.FC<Props> = ({ tabs, data }) => {
             >
               {tabs.map((tab, colIndex) => (
                 <td
-                  style={{ background: tab.background,width:`${tab.width}%` }}
+                  style={{ background: tab.background, width: `${tab.width}%` }}
                   key={colIndex}
-                  className={`${
-                    !tab.background
-                      ? "border  border-[#E2DFDF]"
-                      : i == 0
-                      ? "rounded-tl-[20px] "
-                      : i === data.length - 1 && "rounded-bl-[20px]  bg-red-500"
-                  }  px-4 py-2`}
+                //   className={`${
+                //     !tab.background
+                //       ? "border  border-[#E2DFDF]"
+                //       : i == 0
+                //       ? "rounded-tl-[20px] "
+                //       : i === data.length - 1 && "rounded-bl-[20px]  bg-red-500"
+                //   }  px-4 py-2`}
+		className={clsx("px-4 py-2",i == 0 && colIndex == 0 && "rounded-tl-[20px]",i ===data.length - 1 && colIndex === 0 && "rounded-bl-[20px]",colIndex!= 0 && colIndex<tabs.length -1 && "border",colIndex === tabs.length-1 && i != data.length -1 && "border-b border-l" )}
                 >
                   {rowData[tab.title] == true && tab.title == "Confirmed" ? (
                     <div className="text-green-500">{<FaRegCheckCircle />}</div>
